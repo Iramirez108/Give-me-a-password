4e33ddb20f80291003b588836ebdb2e77a5afc78
Iramirez108 <ivan.ramirez10208@gmail.com>
2022-06-28 23:05:59 -0500

    read me and scrip change

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Give-me-a-password
+
+First thing I had to do was define all the characters on the keyboard that 
+will allow them to use for a password.
+
--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numberChar = "0123456789";
-var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
+var lowercaseChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var uppercaseChar = [  'A', 'B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numberChar = ['0','1','2','3','4','5','6','7','8','9'];
+var specialChar = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
 
 // Prompt the user for the password criteria
   // Password lenght has to be between 8 to 128 characters
   // lowercase,uppercase, numbers, special characters
   // Validate the input 
   // Display generated password on the page or alert)
+
+  function getRandom(arr) {
+   
+  }
+
+
 function generatePassword(){
   console.log("Yay! You clicked the button!")
-  var password = "";
-  var passwordChar = "";
+  var password = [];
+  var passwordChar = [];
   let passwordLengthUser = prompt("How long do you want your password to be?");
   passwordLengthUser = parseInt(passwordLengthUser);
   if (passwordLengthUser <8){
     alert("Password must have more than 8 characters!!");
-    return"";
+    return "";
   }
   if (passwordLengthUser > 128){
     alert("password must not have more than 128 characters!!");
@@ -27,24 +57,32 @@ function generatePassword(){
   }
   var lowercaseCharactersChoice = confirm ("Do you want to include lowercase in your password?");
   if (lowercaseCharactersChoice){
-    passwordChar += lowercaseChar
+    password = password.concat(lowercaseChar);
+   passwordChar.push(getRandom(lowercaseChar));
+    console.log(lowercaseChar)
   }
   var uppercaseCharactersChoice = confirm ("Do you want to include uppercase in your password?");
   if(uppercaseCharactersChoice){
-    passwordChar += uppercaseChar;
+    password = password.concat(uppercaseChar);
+    passwordChar.push(getRandom(uppercaseChar));
+    console.log(uppercaseChar)
   }
   var numberCharChoice = confirm ("Do you want to add numbers in your password?");
   if (numberCharChoice){
-    passwordChar += numberChar;
+    password = password.concat(numberChar);
+    passwordChar.push(getRandom(numberChar));
+    console.log(numberChar)
   }
   var specialCharChoice = confirm ("Do you want to add special characters to your password?");
   if (specialCharChoice){
-    passwordChar += specialChar;
+    password = password.concat(specialChar);
+    passwordChar.push(getRandom(specialChar));
+    console.log(specialChar)
   }
   for (var i=0;i<passwordLengthUser; i++){
     password = passwordChar[Math.floor(Math.random()*passwordChar.length)]
   }
-  return password
+  return password.join();
 }
 
 
@@ -58,7 +96,6 @@ function writePassword() {
 
   if (passwordText !=null){
     document.querySelector("#password").innerHTML = password;
- // document.querySelector("password").innerHTML = password;
 }
 }
 
